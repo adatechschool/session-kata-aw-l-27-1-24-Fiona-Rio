e46de77a490e06de19d62120ace333ffaee5c4e0
Fiona RIO <fiona.rio@outlook.com>
2025-01-27 11:25:38 +0100

    Add files via upload

---
+++ awele.js
@@ -0,0 +1,16 @@
+console.log("Bonjour")
+const plateau = {
+    constructor() {
+        this.case = "( )";
+        this.lettres = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"];
+    }
+};
+
+function display() {
+    console.log()
+
+}
+
+function isEmpy() {
+
+}
